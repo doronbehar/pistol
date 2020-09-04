7aa089555b3f7467126ef41cba458e2997e75e41
Doron Behar <doron.behar@gmail.com>
2020-09-04 10:21:15 +0300

    Make previewer's api docs better.
    Use more uppercase in Previewer struct so these elements will be
    exposed.

--- previewer.go
+++ previewer.go
@@ -17,14 +17,39 @@ import (
 	"github.com/rakyll/magicmime"
 )
 
+// A type NewPreviewer returns
 type Previewer struct {
-	filePath string
-	mimeType string
-	// if the following are set, we use them, if not, we revert to using internal mechanisms
-	command string
-	args []string
+	// The file to be previewed
+	FilePath string
+	// The mime type detected
+	MimeType string
+	// The command that will be used to print the file. If empty, internal
+	// writers are used. Concluded by the configuration file passed to
+	// NewPreviewer.
+	Command string
+	// The arguments to the command
+	Args []string
 }
 
+// Return a new Previewer that can .Write to an io.Writer.
+//
+//   * `filePath` is the file you'd like to preview.
+//   * `configPath` should point to a configuration file in the format as explained
+//   here: https://github.com/doronbehar/pistol#configuration
+//
+// You can set the environmental variable `PISTOL_DEBUG` to any non empty value
+// and it will instruct NewPreviewer to spit additional log messages when
+// parsing configPath and detecting the mime type of `filePath`. If configPath
+// is an empty string, a warning message will be printed to stderr,
+// unconditionally.
+//
+// `pistol`, the command line tool, searches for a default configuration file
+// in ~/.config/pistol/pistol.conf. The API doesn't include this functionality.
+//
+// Mime type detection is provided by libmagic (through github.com/rakyll/magicmime)
+//
+// Many mime types are handled internally by Pistol, see table here:
+// https://github.com/doronbehar/pistol#introduction
 func NewPreviewer(filePath, configPath string) (Previewer, error) {
 	verbose := os.Getenv("PISTOL_DEBUG")
 	if verbose != "" {
@@ -44,9 +69,9 @@ func NewPreviewer(filePath, configPath string) (Previewer, error) {
 	if err != nil {
 		return p, err
 	}
-	p.mimeType = mimetype
-	log.Infof("detected mimetype is %s", p.mimeType)
-	p.filePath = filePath
+	p.MimeType = mimetype
+	log.Infof("detected mimetype is %s", p.MimeType)
+	p.FilePath = filePath
 	// If configuration file doesn't exist, we don't try to read it
 	if configPath == "" {
 		log.Warnf("configuration file was not supplied")
@@ -65,13 +90,13 @@ func NewPreviewer(filePath, configPath string) (Previewer, error) {
 			// Empty lines are skipped
 			continue
 		}
-		match, err := regexp.MatchString(def[0], p.mimeType)
+		match, err := regexp.MatchString(def[0], p.MimeType)
 		if err != nil {
 			return p, err
 		}
 		if match && len(def) > 1 {
-			p.command = def[1]
-			p.args = def[2:]
+			p.Command = def[1]
+			p.Args = def[2:]
 			return p, nil
 		}
 		match, err = regexp.MatchString("^#", def[0])
@@ -105,49 +130,51 @@ func NewPreviewer(filePath, configPath string) (Previewer, error) {
 		}
 		if match {
 			log.Infof("matched file path against absFpath: %s", absFpath)
-			p.command = def[2]
-			p.args = def[3:]
+			p.Command = def[2]
+			p.Args = def[3:]
 			return p, nil
 		}
 	}
-	log.Infof("didn't find a match in configuration for detected mimetype: %s\n", p.mimeType)
+	log.Infof("didn't find a match in configuration for detected mimetype: %s\n", p.MimeType)
 	return p, nil
 }
 
+// Write to an io.Writer the Previewer's text - concluded mostly by
+// NewPreviewer
 func (p *Previewer) Write(w io.Writer) (error) {
 	// if a match was encountered when the configuration file was read
-	if p.command != "" {
-		if match, _ := regexp.MatchString("%pistol-filename%", strings.Join(p.args, " ")); !match {
+	if p.Command != "" {
+		if match, _ := regexp.MatchString("%pistol-filename%", strings.Join(p.Args, " ")); !match {
 			return errors.New("no %pistol-filename% found in definition command")
 		}
 		var replStr string
-		if p.command == "sh:" {
-			replStr = she.Quote(p.filePath)
+		if p.Command == "sh:" {
+			replStr = she.Quote(p.FilePath)
 		} else {
-			replStr = p.filePath
+			replStr = p.FilePath
 		}
 		var cmd *exec.Cmd
 		var argsOut []string
-		for _, arg := range p.args {
+		for _, arg := range p.Args {
 			argsOut = append(argsOut, strings.ReplaceAll(arg, "%pistol-filename%", replStr))
 		}
-		if p.command == "sh:" {
+		if p.Command == "sh:" {
 			log.Infof("previewer's command is (shell interpreted): %#v\n", argsOut)
 			cmd = exec.Command("sh", "-c", strings.Join(argsOut, " "))
 		} else {
-			log.Infof("previewer's command is (no shell) %#v with args: %#v\n", p.command, argsOut)
-			cmd = exec.Command(p.command, argsOut...)
+			log.Infof("previewer's command is (no shell) %#v with args: %#v\n", p.Command, argsOut)
+			cmd = exec.Command(p.Command, argsOut...)
 		}
 		cmd.Stdout = w
 		cmd.Stderr = os.Stderr
 		if err := cmd.Start(); err != nil {
-			log.Fatalf("We've had issues running your command: %v, %s", p.command, p.args)
+			log.Fatalf("We've had issues running your command: %v, %s", p.Command, p.Args)
 			return err
 		}
 		cmd.Wait()
 	} else {
 		// try to match with internal writers
-		internal_writer, err := pistol.MatchInternalWriter(p.mimeType, p.filePath)
+		internal_writer, err := pistol.MatchInternalWriter(p.MimeType, p.FilePath)
 		if err != nil {
 			return err
 		}
