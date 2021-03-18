2e645a020d998ef68d0c824f878a3e95ed0f852a
Doron Behar <doron.behar@gmail.com>
2021-03-18 08:24:31 +0200

    Add pistol-static and more
    Use the same nixpkgs input for gomod2nix.

--- flake.nix
+++ flake.nix
@@ -2,16 +2,19 @@
   description = "General purpose file previewer designed for Ranger, Lf to make scope.sh redundant";
 
   # To make user overrides of the nixpkgs flake not take effect
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  # https://nixos.wiki/wiki/Flakes#Using_flakes_project_from_a_legacy_Nix
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # https://nixos.wiki/wiki/Flakes#Using_flakes_project_from_a_legacy_Nix
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    gomod2nix-flake = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-
-  inputs.gomod2nix-flake.url = "github:tweag/gomod2nix";
 
   outputs = { self
     , nixpkgs
@@ -28,6 +31,18 @@
         inherit system;
         config = {};
       };
+      nativeBuildInputs = [
+        pkgs.gomod2nix
+      ];
+      checkInputs = [
+        pkgs.elinks
+        pkgs.bat
+        # For tput
+        pkgs.ncurses
+      ];
+      buildInputs = [
+        pkgs.file
+      ];
       pistol = pkgs.buildGoApplication rec {
         pname = "pistol";
         version = "${pkgs.lib.strings.removeSuffix "\n" (builtins.readFile ./VERSION)}-flake";
@@ -35,29 +50,36 @@
           (path: type: type != "directory" || baseNameOf path != ".git")
           ./.;
         modules = ./gomod2nix.toml;
-        inherit (pkgs.pistol)
-          meta
-          passthru
-          subPackages
-          buildInputs
-        ;
+        inherit (pkgs.pistol) meta;
+        subPackages = [ "cmd/pistol" ];
+        # We don't do a check in every build (yet) so we don't need to inherit
+        # checkInputs as of yet.
+        inherit buildInputs;
+        CGO_ENABLED = "1";
         buildFlagsArray = [ "-ldflags=-s -w -X main.Version=${version}" ];
       };
-    in rec {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.file
-          pkgs.elinks
-          pkgs.gomod2nix
+      pistol-static = pistol.overrideAttrs(oldAttrs: {
+        pname = "pistol-static";
+        buildInputs = buildInputs ++ [
+          pkgs.glibc.static
+          pkgs.pkgsStatic.file
         ];
+        CFLAGS = "-I${pkgs.glibc.dev}/include";
+        LDFLAGS = "-I${pkgs.glibc}/lib";
+      });
+    in {
+      devShell = pkgs.mkShell {
+        inherit buildInputs nativeBuildInputs checkInputs;
       };
-      packages.pistol = pistol;
+      packages = {
+        inherit pistol pistol-static;
+      };
       defaultPackage = pistol;
       apps.pistol = {
         type = "app";
         program = "${pistol}/bin/pistol";
       };
-      defaultApp = apps.pistol;
+      defaultApp = self.apps.pistol;
     }
   );
 }
