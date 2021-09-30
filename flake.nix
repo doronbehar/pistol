5462413d103d585d300ab6783d1ece8486b6c714
Doron Behar <doron.behar@gmail.com>
2021-09-30 17:09:13 +0300

    Use gomodnix to build pistol

--- flake.nix
+++ flake.nix
@@ -10,28 +10,81 @@
     url = "github:edolstra/flake-compat";
     flake = false;
   };
+  inputs.gomod2nix = {
+    url = "github:tweag/gomod2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+    inputs.utils.follows = "flake-utils";
+  };
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   outputs = { self
     , nixpkgs
     , flake-utils
     , flake-compat
+    , gitignore
+    , gomod2nix
   }:
   flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
-      pistol = pkgs.pistol.overrideAttrs(oldAttrs: rec {
-        version = "${builtins.readFile ./VERSION}-flake";
-        src = builtins.filterSource
-          (path: type: type != "directory" || baseNameOf path != ".git")
-          ./.;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          gomod2nix.overlay
+        ];
+      };
+      inherit (gitignore.lib) gitignoreSource;
+      # https://discourse.nixos.org/t/passing-git-commit-hash-and-tag-to-build-with-flakes/11355/2
+      version_rev = if (self ? rev) then (builtins.substring 0 8 self.rev) else "dirty";
+      version = "${pkgs.lib.fileContents ./VERSION}-${version_rev}-flake";
+      pistol = pkgs.buildGoApplication {
+        pname = "pistol";
+        inherit version;
+        src = pkgs.lib.cleanSourceWith {
+          # Ignore many files that gitignoreSource doesn't ignore, see:
+          # https://github.com/hercules-ci/gitignore.nix/issues/9#issuecomment-635458762
+          filter = path: type:
+          ! (builtins.any (r: (builtins.match r (builtins.baseNameOf path)) != null) [
+            # Nix files
+            "flake.nix"
+            "flake.lock"
+            "default.nix"
+            "shell.nix"
+            ".envrc"
+            # Evaluated but not used for the build itself
+            "gomod2nix.toml"
+            "VERSION"
+            # CI files
+            "renovate.json5"
+            # Git files
+            ".gitignore"
+            ".git"
+          ])
+          ;
+          src = gitignoreSource ./.;
+        };
         buildFlagsArray = [
           "-ldflags=-s -w -X main.Version=${version}"
         ];
-      });
+        modules = ./gomod2nix.toml;
+        inherit (pkgs.pistol)
+          nativeBuildInputs
+          buildInputs
+          subPackages
+          postBuild
+          meta
+        ;
+        CGO_ENABLED = 1;
+      };
     in rec {
       devShell = pkgs.mkShell {
-        buildInputs = pistol.buildInputs ++ [
+        inherit (pistol) buildInputs;
+        nativeBuildInputs = pistol.nativeBuildInputs ++ [
+          # For make check
           pkgs.elinks
+          pkgs.gomod2nix
         ];
       };
       packages.pistol = pistol;
