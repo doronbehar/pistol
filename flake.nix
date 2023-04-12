694b47cdb47e51bacce5095d16a25889cf564946
Doron Behar <doron.behar@gmail.com>
2023-04-12 18:05:01 +0300

    Add cross compiled packages

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
   };
   inputs.gomod2nix = {
     # For static compilation I need: https://github.com/tweag/gomod2nix/pull/24
-    url = "github:doronbehar/gomod2nix/go-stdenv";
+    url = "github:nix-community/gomod2nix";
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.utils.follows = "flake-utils";
   };
@@ -33,101 +33,46 @@
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          gomod2nix.overlay
+          gomod2nix.overlays.default
         ];
       };
-      inherit (gitignore.lib) gitignoreSource;
+      inherit (gitignore.lib) gitignoreFilterWith;
       # https://discourse.nixos.org/t/passing-git-commit-hash-and-tag-to-build-with-flakes/11355/2
       version_rev = if (self ? rev) then (builtins.substring 0 8 self.rev) else "dirty";
       version = "${pkgs.lib.fileContents ./VERSION}-${version_rev}-flake";
-      # Create the buildGoApplication variants
-      inherit (pkgs) buildGoApplication;
-      buildGoApplicationStatic = buildGoApplication.override {
-        stdenv = pkgs.pkgsStatic.stdenv;
-      };
       # Used also in the devShell
       MAGIC_DB = "${pkgs.pkgsStatic.file}/share/misc/magic.mgc";
-      # arguments used in many derivation arguments calls
-      common-drv-args = {
-        pname = "pistol";
-        inherit version;
-        src = pkgs.lib.cleanSourceWith {
-          # Ignore many files that gitignoreSource doesn't ignore, see:
-          # https://github.com/hercules-ci/gitignore.nix/issues/9#issuecomment-635458762
-          filter = path: type:
-          ! (builtins.any (r: (builtins.match r (builtins.baseNameOf path)) != null) [
-            # Nix files
-            "flake.nix"
-            "flake.lock"
-            "default.nix"
-            "shell.nix"
-            ".envrc"
+      src = pkgs.lib.cleanSourceWith {
+        filter = gitignoreFilterWith {
+          basePath = ./.;
+          extraRules = ''
+            flake*
+            *.nix
+            ./azure-pipelines.yml
+            .envrc
             # Evaluated but not used for the build itself
-            "gomod2nix.toml"
-            "VERSION"
+            VERSION
             "bump-version.sh"
             # CI files
-            "renovate.json5"
+            renovate.json5
             # Git files
-            ".gitignore"
-            ".git"
-          ])
-          ;
-          src = gitignoreSource ./.;
+            .gitignore
+          '';
         };
-        buildFlagsArray = ''
-          -ldflags=
-          -X main.Version=${version}
-        '';
-        modules = ./gomod2nix.toml;
-        inherit (pkgs.pistol)
-          nativeBuildInputs
-          subPackages
-          postBuild
-          meta
-        ;
-        CGO_ENABLED = 1;
+        src = ./.;
       };
-      common-static-drv-args = (common-drv-args // {
-        nativeBuildInputs = common-drv-args.nativeBuildInputs ++ [
-          pkgs.removeReferencesTo
-        ];
-        # From some reason even though zlib is static we need this, but it
-        # doesn't create a real reference to zlib.
-        NIX_LDFLAGS = "-lz";
-        preBuild = ''
-          cp ${MAGIC_DB} ./cmd/pistol/magic.mgc
-        '';
-        buildFlags = ''
-          -tags EMBED_MAGIC_DB
-        '';
-        postFixup = ''
-          # Remove unnecessary references to zlib.
-          rm -r $out/nix-support
-          # Remove more unnecessary references which I don't know the source of
-          # which. I guess they are due to features of some go modules I don't
-          # use.
-          remove-references-to -t ${pkgs.mailcap} $out/bin/pistol
-          remove-references-to -t ${pkgs.iana-etc} $out/bin/pistol
-          remove-references-to -t ${pkgs.tzdata} $out/bin/pistol
-        '';
-      });
-      pistol = buildGoApplication (common-drv-args // {
-        inherit (pkgs.pistol) buildInputs;
-      });
-      pistol-static = buildGoApplicationStatic (common-static-drv-args // {
-        buildInputs = [
-          pkgs.pkgsStatic.file
-          pkgs.pkgsStatic.zlib
-        ];
-        postFixup = common-static-drv-args.postFixup + ''
-          remove-references-to -t ${pkgs.pkgsStatic.file} $out/bin/pistol
-        '';
-      });
-    in rec {
+      pkgArgs = {
+        inherit version src;
+      };
+      pistol = pkgs.callPackage ./pkg.nix pkgArgs;
+      pistol-static = pkgs.pkgsStatic.callPackage ./pkg.nix pkgArgs;
+      pistol-static-aarch64 = pkgs.pkgsCross.aarch64-multiplatform-musl.pkgsStatic.callPackage ./pkg.nix pkgArgs;
+      pistol-static-armv7l = pkgs.pkgsCross.armv7l-hf-multiplatform.pkgsStatic.callPackage ./pkg.nix pkgArgs;
+      pistol-static-arm = pkgs.pkgsCross.arm-embedded.pkgsStatic.callPackage ./pkg.nix pkgArgs;
+    in {
       devShell = pkgs.mkShell {
-        inherit (pistol) buildInputs;
-        nativeBuildInputs = pistol.nativeBuildInputs ++ [
+        nativeBuildInputs = [
+          pkgs.file
           # For make check
           pkgs.elinks
           pkgs.gomod2nix
@@ -138,6 +83,9 @@
         inherit
           pistol
           pistol-static
+          pistol-static-aarch64
+          pistol-static-armv7l
+          pistol-static-arm
         ;
       };
       defaultPackage = pistol;
@@ -145,7 +93,7 @@
         type = "app";
         program = "${pistol}/bin/pistol";
       };
-      defaultApp = apps.pistol;
+      defaultApp = self.apps.${system}.pistol;
     }
   );
 }
---
+++ pkg.nix
@@ -0,0 +1,57 @@
+{ lib
+, stdenv
+, buildGoApplication
+, version, src
+, file
+, buildPackages
+, removeReferencesTo
+, installShellFiles
+, asciidoctor
+}:
+
+buildGoApplication {
+  pname = "pistol";
+  inherit version src;
+  pwd = ./.;
+  modules = ./gomod2nix.toml;
+
+  subPackages = [ "cmd/pistol" ];
+  ldflags = [ "-s" "-w" "-X main.Version=${version}" ];
+
+  doCheck = false;
+  buildInputs = [
+    file
+  ];
+  nativeBuildInputs = [
+    installShellFiles
+    asciidoctor
+  ] ++ lib.optionals stdenv.hostPlatform.isStatic [
+    removeReferencesTo
+  ];
+  NIX_LDFLAGS = lib.optionalString stdenv.hostPlatform.isStatic "-lz";
+  tags = lib.optionals stdenv.hostPlatform.isStatic [
+    "EMBED_MAGIC_DB"
+  ];
+  preBuild = lib.optionalString stdenv.hostPlatform.isStatic ''
+    cp ${file}/share/misc/magic.mgc ./cmd/pistol/magic.mgc
+  '';
+  postInstall = ''
+    asciidoctor -b manpage -d manpage README.adoc
+    installManPage pistol.1
+  '';
+  postFixup = lib.optionalString stdenv.hostPlatform.isStatic ''
+    # Remove unnecessary references to zlib.
+    rm -r $out/nix-support
+    # Remove more unnecessary references which I don't know the source of
+    # which. I guess they are due to features of some go modules I don't
+    # use.
+    remove-references-to -t ${buildPackages.mailcap} $out/bin/pistol
+    remove-references-to -t ${buildPackages.iana-etc} $out/bin/pistol
+    remove-references-to -t ${buildPackages.tzdata} $out/bin/pistol
+  '';
+  meta = {
+    description = "General purpose file previewer designed for Ranger, Lf to make scope.sh redundant";
+    homepage = "https://github.com/doronbehar/pistol";
+    license = lib.licenses.mit;
+  };
+}
